93977737c51d9dd46c7383bbc3190f779529c467
ebith <ebith.h@gmail.com>
2026-08-08 00:53:49 +0900

    Update benchmark

--- README.md
+++ README.md
@@ -15,5 +15,4 @@ Discordにこんな感じに流れてくる。
 - Gemini Flash Lite系の無料枠が15RPM/500RPDなのでこのツールで使う程度だと無料でいけそう
 - Gemini 3.1 Flash-Lite Previewだと入力545 出力100-220トークンみたいな感じになるので1回0.05-0.08円
 - Gemini 2.5 Flashだと入力276 出力200+思考トークンという感じで1回0.3円前後
-- 単発のスクリーンショット目的だとGDI([XCap](https://github.com/nashaofu/xcap))の方がWGC([wgc](https://github.com/Atliac/wgc))よりだいぶ早かった
-  - wgcのexamplesに従うと画面の縁がチカチカしちゃう問題もある
+- 単発のスクリーンショット目的だとWGC([wgc](https://github.com/Atliac/wgc))よりGDI([XCap](https://github.com/nashaofu/xcap))の方が気持ち速くかつ圧倒的に安定している
--- benches/capture.rs
+++ benches/capture.rs
@@ -3,23 +3,6 @@ use image::{ImageBuffer, Rgba};
 use windows::Win32::{Graphics::Gdi::MonitorFromWindow, UI::WindowsAndMessaging::GetForegroundWindow};
 use xcap::Monitor;
 
-fn capture_with_wgc() -> Result<(), Box<dyn std::error::Error>> {
-    let item = wgc::new_item_from_monitor(unsafe {
-        MonitorFromWindow(GetForegroundWindow(), windows::Win32::Graphics::Gdi::MONITOR_DEFAULTTONEAREST)
-    })?;
-
-    let wgc = wgc::Wgc::new(item, Default::default())?;
-
-    for frame in wgc.take(1) {
-        let frame = frame?;
-        let frame_size = frame.size()?;
-        let buffer = frame.read_pixels(None)?;
-        let _image: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::from_raw(frame_size.width, frame_size.height, buffer).unwrap();
-    }
-
-    Ok(())
-}
-
 fn capture_with_gdi() {
     let monitors = Monitor::all().unwrap();
     let _image = monitors[0].capture_image().unwrap();
@@ -27,7 +10,20 @@ fn capture_with_gdi() {
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Capture with GDI", |b| b.iter(|| capture_with_gdi()));
-    c.bench_function("Capture with WGC", |b| b.iter(|| capture_with_wgc()));
+
+    let item = wgc::new_item_from_monitor(unsafe {
+        MonitorFromWindow(GetForegroundWindow(), windows::Win32::Graphics::Gdi::MONITOR_DEFAULTTONEAREST)
+    }).unwrap();
+    let mut wgc = wgc::Wgc::new(item, Default::default()).unwrap();
+
+    c.bench_function("Capture with WGC", |b| b.iter(|| {
+        if let Some(frame_result) = wgc.next() {
+            let frame = frame_result.unwrap();
+            let frame_size = frame.size().unwrap();
+            let buffer = frame.read_pixels(None).unwrap();
+            let _image: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::from_raw(frame_size.width, frame_size.height, buffer).unwrap();
+        }
+    }));
 }
 
 criterion_group!(benches, criterion_benchmark);
