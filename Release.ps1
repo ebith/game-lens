335281c663dfb013be717bbba4c076eb30ee5b99
ebith <ebith.h@gmail.com>
2026-08-27 22:00:21 +0900

    Update script

--- Release.ps1
+++ Release.ps1
@@ -1,6 +1,11 @@
 $ErrorActionPreference = "Stop"
 
 $DistDir = "dist\game-lens"
+$Archive = "dist\game-lens.7z"
+$Tag = "v" + (Get-Date -Format "yyyyMMdd")
+
+cargo build --release
+
 if (Test-Path $DistDir) { Remove-Item -Recurse -Force $DistDir }
 New-Item -ItemType Directory -Force -Path $DistDir | Out-Null
 
@@ -8,5 +13,7 @@ Copy-Item "target\release\game-lens.exe" -Destination $DistDir
 Copy-Item "config.toml" -Destination $DistDir
 Copy-Item "Start.ps1" -Destination $DistDir
 
-if (Test-Path "dist\game-lens.7z") { Remove-Item -Force "dist\game-lens.7z" }
-& 7z.exe a -t7z -mx7 "dist\game-lens.7z" ".\$DistDir\*"
+if (Test-Path $Archive) { Remove-Item -Force $Archive }
+& 7z.exe a -t7z -mx7 $Archive ".\$DistDir\*"
+
+gh release create $Tag $Archive --generate-notes
